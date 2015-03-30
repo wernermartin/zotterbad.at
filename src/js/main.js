c59f4f86c3ad6d83f509f745a6cf186445be37f5
Martin Werner <git@martinwerner.at>
2015-03-30 13:18:07 +0200

    subnavigation jquery, content

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,20 @@
-function subNavigation() {
-	var span = document.querySelectorAll('span'),
-    element = document.getElementById('xyz'),
-    data = element.getAttribute("data-sub-nav");
+function subNavigation(elID) {
+	
+	console.log(elID);
 
-    console.log(data);
+	$("#main_navigation>li>a").removeClass("nav-active");
+	$("#" + elID).addClass("nav-active");
+
+	var subNav = "sub" + elID;
+	$("#sub_navigation>div.active").removeClass("active");
+	$("#" + subNav).addClass("active");
+
 }
 
 
-subNavigation();
-
+$(document).ready(function() {
+    $('#nav_home, #nav_aboutzotter, #nav_bath, #nav_energy, #nav_zotterpoints, #nav_news').click(function(event) {
+        subNavigation(event.target.id);
+    });
+  });
 
